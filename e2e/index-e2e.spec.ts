096687570c867b7f6cbe586518ba9ba2ef7d8c88
trik <marco.marche@gnucoop.com>
2018-01-24 15:23:21 +0100

    fix(e2e): take screenshot in dummy test

--- e2e/index-e2e.spec.ts
+++ e2e/index-e2e.spec.ts
@@ -1,10 +1,12 @@
 import {browser} from 'protractor';
+import {screenshot} from './screenshot';
 
 describe('hello, protractor', () => {
   describe('index', () => {
     browser.get('/');
     it('should have a title', async () => {
       expect(await browser.getTitle()).toBe('Ng-Extra');
+      screenshot('hello protractor');
     });
   });
 });
